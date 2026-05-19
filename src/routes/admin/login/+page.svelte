346d5b998997629081421ffee0c7e33792b92ae6
Daringcuteseal <daringcuteseal@gmail.com>
2026-05-19 23:11:06 +0700

    remove unused import

--- src/routes/admin/login/+page.svelte
+++ src/routes/admin/login/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { Button } from "$lib/components/ui/button/index.js";
-	import { Label } from "$lib/components/ui/label/index.js";
 	import { Input } from "$lib/components/ui/input/index.js";
 	import * as Card from "$lib/components/ui/card/index.js";
 	import * as Field from "$lib/components/ui/field/index.js";
