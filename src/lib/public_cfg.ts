ba7c7489ec69d14cbb6fda3ea50226ae576deff2
Daringcuteseal <daringcuteseal@gmail.com>
2026-05-27 20:00:57 +0700

    add website title for branding

--- src/lib/public_cfg.ts
+++ src/lib/public_cfg.ts
@@ -1,4 +1,5 @@
 import { PUBLIC_FLOATIMER_TIMEZONE } from "$env/static/public";
+import { PUBLIC_TITLE } from "$env/static/public";
 
 if (!PUBLIC_FLOATIMER_TIMEZONE || typeof PUBLIC_FLOATIMER_TIMEZONE != "string") {
 	throw new Error("Missing FLOATIMER_TIMEZONE");
@@ -6,8 +7,10 @@ if (!PUBLIC_FLOATIMER_TIMEZONE || typeof PUBLIC_FLOATIMER_TIMEZONE != "string") 
 
 export type PublicConfig = {
 	TIMEZONE: string
+	TITLE: string
 }
 
 export const public_cfg: PublicConfig = {
-	TIMEZONE: PUBLIC_FLOATIMER_TIMEZONE
+	TIMEZONE: PUBLIC_FLOATIMER_TIMEZONE,
+	TITLE: !PUBLIC_TITLE ? "Floatimer" : PUBLIC_TITLE
 };
