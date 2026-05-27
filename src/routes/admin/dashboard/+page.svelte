390198aadf1b5db4a40907708cb11961c38521fc
Daringcuteseal <daringcuteseal@gmail.com>
2026-05-27 08:11:25 +0700

    add theme toggle in admin dashboard

--- src/routes/admin/dashboard/+page.svelte
+++ src/routes/admin/dashboard/+page.svelte
@@ -9,6 +9,8 @@
 	import * as Dialog from "$lib/components/ui/dialog/index.js";
 	import { Button } from "$lib/components/ui/button/index.js";
 	import Settings from "@lucide/svelte/icons/settings";
+	import SunIcon from "@lucide/svelte/icons/sun";
+	import MoonIcon from "@lucide/svelte/icons/moon";
 	import CircleSmall from "@lucide/svelte/icons/circle-small";
 	import Info from "@lucide/svelte/icons/info";
 	import { goto, invalidateAll } from "$app/navigation";
@@ -240,6 +242,16 @@
 				</Breadcrumb.List>
 			</Breadcrumb.Root>
 			<div class="ms-auto flex items-center gap-2">
+				<Button onclick={toggleMode} variant="outline" size="icon">
+					<SunIcon
+						class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
+					/>
+					<MoonIcon
+						class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0"
+					/>
+					<span class="sr-only">Toggle theme</span>
+				</Button>
+
 				<Dialog.Root>
 					<Dialog.Trigger class={buttonVariants({ variant: "outline" })}
 						><Info /> Server</Dialog.Trigger
