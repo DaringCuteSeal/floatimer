97261b2bac0302d3602fdbd0cd7a674f27bfc3cd
Daringcuteseal <daringcuteseal@gmail.com>
2026-05-27 20:00:40 +0700

    add bg image to home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
 	import * as Popover from "$lib/components/ui/popover";
 	import { Calendar } from "$lib/components/ui/calendar";
 	import CircleSmall from "@lucide/svelte/icons/circle-small";
+	import Watermark from "$lib/components/ui/watermark/watermark.svelte";
 	import {
 		CalendarDate,
 		fromDate,
@@ -47,7 +48,6 @@
 	});
 </script>
 
-
 <ModeWatcher />
 <Button
 	onclick={toggleMode}
@@ -64,109 +64,141 @@
 	<span class="sr-only">Toggle theme</span>
 </Button>
 
-<div class="flex flex-col m-5 gap-5">
-	<div class="flex justify-center items-center">
-		<Popover.Root bind:open={calendarOpen}>
-			<Popover.Trigger id="date">
-				{#snippet child({ props })}
-					<Button
-						{...props}
-						variant="outline"
-						class="w-40 justify-between font-normal"
-					>
-						<CalendarIcon />
-						{calValue
-							? calValue.toDate(public_cfg.TIMEZONE).toLocaleDateString()
-							: "Select date"}
-						<ChevronDown />
-					</Button>
-				{/snippet}
-			</Popover.Trigger>
-			<Popover.Content class="w-auto overflow-hidden p-0" align="start">
-				<Card.Root class="max-w-xl w-full gap-5">
-					<Card.Header>
-						<Card.Title>Tanggal</Card.Title>
-						<Card.Description>Pilih tanggal.</Card.Description>
-						<Card.Action>
-							<Button
-								size="sm"
-								variant="outline"
-								onclick={() => (calValue = today(public_cfg.TIMEZONE))}
-							>
-								Hari ini
-							</Button>
-						</Card.Action>
-					</Card.Header>
-					<Card.Content>
-						<Calendar
-							locale="id-ID"
-							captionLayout="dropdown"
-							value={calValue}
-							type="single"
-							class="w-full rounded-md [&_table]:w-full [&_td]:w-full [&_th]:w-full"
-							onValueChange={(newDate) => {
-								if (!newDate) {
-									return;
-								}
-								calValue = toCalendarDate(newDate);
-							}}
-							minValue={thisDay}
+<div class="min-h-screen flex flex-col bg">
+	<div class="flex flex-col m-5 gap-5 items-center flex-1">
+		<div class="flex justify-center items-center">
+			<Popover.Root bind:open={calendarOpen}>
+				<Popover.Trigger id="date">
+					{#snippet child({ props })}
+						<Button
+							{...props}
+							variant="outline"
+							class="w-40 justify-between font-normal"
 						>
-							{#snippet day({ day })}
-								{@const existsDate = data.dateWithTimers.has(
-									dateValueFormatMachine(day),
-								)}
-								<CalendarDay class="flex flex-col items-center">
-									{day.day}
-									{#if existsDate && day.compare(thisDay) >= 0}
-										<span>
-											<CircleSmall fill="white" size={10} />
-										</span>
-									{/if}
-								</CalendarDay>
-							{/snippet}
-						</Calendar>
-					</Card.Content>
-				</Card.Root>
-			</Popover.Content>
-		</Popover.Root>
-	</div>
-	{#if data.timers.length > 0}
-		<div
-			class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5 overflow-x-auto items-start"
-		>
-			{#each data.timers as timer (timer.id)}
-				<Card.Root>
-					<Card.Header>
-						<Card.Title>
-							{#if timer.name == ""}
-								<i>Timer Tanpa Nama</i>
-							{:else}
-								{timer.name}
-							{/if}
-						</Card.Title>
-						<Card.Description>
-							{timer.subject?.name}
-						</Card.Description>
-					</Card.Header>
-					<Card.Content>
-						<p>
-							Waktu:
-							{String(timer.time_start.getHours()).padStart(
-								2,
-								"0",
-							)}:{String(timer.time_start.getMinutes()).padStart(2, "0")}
-							-
-							{String(timer.time_end.getHours()).padStart(
-								2,
-								"0",
-							)}:{String(timer.time_end.getMinutes()).padStart(2, "0")}
-						</p>
-					</Card.Content>
-				</Card.Root>
-			{/each}
+							<CalendarIcon />
+							{calValue
+								? calValue
+										.toDate(public_cfg.TIMEZONE)
+										.toLocaleDateString()
+								: "Select date"}
+							<ChevronDown />
+						</Button>
+					{/snippet}
+				</Popover.Trigger>
+				<Popover.Content class="w-auto overflow-hidden p-0" align="start">
+					<Card.Root class="max-w-xl w-full gap-5">
+						<Card.Header>
+							<Card.Title>Tanggal</Card.Title>
+							<Card.Description>Pilih tanggal.</Card.Description>
+							<Card.Action>
+								<Button
+									size="sm"
+									variant="outline"
+									onclick={() =>
+										(calValue = today(public_cfg.TIMEZONE))}
+								>
+									Hari ini
+								</Button>
+							</Card.Action>
+						</Card.Header>
+						<Card.Content>
+							<Calendar
+								locale="id-ID"
+								captionLayout="dropdown"
+								value={calValue}
+								type="single"
+								class="w-full rounded-md [&_table]:w-full [&_td]:w-full [&_th]:w-full"
+								onValueChange={(newDate) => {
+									if (!newDate) {
+										return;
+									}
+									calValue = toCalendarDate(newDate);
+								}}
+								minValue={thisDay}
+							>
+								{#snippet day({ day })}
+									{@const existsDate = data.dateWithTimers.has(
+										dateValueFormatMachine(day),
+									)}
+									<CalendarDay class="flex flex-col items-center">
+										{day.day}
+										{#if existsDate && day.compare(thisDay) >= 0}
+											<span>
+												<CircleSmall fill="white" size={10} />
+											</span>
+										{/if}
+									</CalendarDay>
+								{/snippet}
+							</Calendar>
+						</Card.Content>
+					</Card.Root>
+				</Popover.Content>
+			</Popover.Root>
 		</div>
-	{:else}
-		<p class="text-center">Tidak ada timer ujian untuk hari ini. Yay!</p>
-	{/if}
+		{#if data.timers.length > 0}
+			<div
+				class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5 overflow-x-auto items-start"
+			>
+				{#each data.timers as timer (timer.id)}
+					<Card.Root>
+						<Card.Header>
+							<Card.Title>
+								{#if timer.name == ""}
+									<i>Timer Tanpa Nama</i>
+								{:else}
+									{timer.name}
+								{/if}
+							</Card.Title>
+							<Card.Description>
+								{timer.subject?.name}
+							</Card.Description>
+						</Card.Header>
+						<Card.Content>
+							<p>
+								Waktu:
+								{String(timer.time_start.getHours()).padStart(
+									2,
+									"0",
+								)}:{String(timer.time_start.getMinutes()).padStart(
+									2,
+									"0",
+								)}
+								-
+								{String(timer.time_end.getHours()).padStart(
+									2,
+									"0",
+								)}:{String(timer.time_end.getMinutes()).padStart(
+									2,
+									"0",
+								)}
+							</p>
+						</Card.Content>
+					</Card.Root>
+				{/each}
+			</div>
+		{:else}
+			<p class="text-center">Tidak ada timer ujian untuk hari ini. Yay!</p>
+		{/if}
+	</div>
+	<Watermark />
 </div>
+
+<style>
+	:global(.dark) .bg {
+		background: linear-gradient(rgba(0, 0, 0, 0.8)),
+			url("$lib/assets/drops.png");
+
+		background-repeat: no-repeat;
+		background-attachment: fixed;
+		background-size: cover;
+		background-position: center;
+	}
+
+	.bg {
+		background: url("$lib/assets/drops.png");
+		background-repeat: no-repeat;
+		background-attachment: fixed;
+		background-size: cover;
+		background-position: center;
+	}
+</style>
